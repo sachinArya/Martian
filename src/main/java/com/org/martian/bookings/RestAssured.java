9fd3b6a0b8de70b38983dbe90e57abe01dde8df4
SachinArya <sachin.arya@gds.ey.com>
2019-12-07 23:08:29 +0000

    Adding RestAssured Class

---
+++ src/main/java/com/org/martian/bookings/RestAssured.java
@@ -0,0 +1,13 @@
+package com.org.martian.bookings;
+
+public class RestAssured {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
